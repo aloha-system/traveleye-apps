e5af04f9fe0a66c55385c7193dca4960624f8028
sandeeffendi <sande.effendi@gmail.com>
2025-09-13 02:19:14 +0700

    feat: implement app material theme

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:boole_apps/app/app_router.dart';
+import 'package:boole_apps/core/theme/app_theme.dart';
+import 'package:boole_apps/core/theme/util/text_theme_util.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -6,7 +8,13 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = createTextTheme(context, 'Montserrat', 'Poppins');
+    MaterialTheme theme = MaterialTheme(textTheme);
+
     return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: theme.light(),
+      darkTheme: theme.dark(),
       debugShowCheckedModeBanner: false,
       title: 'Boole Mobile Application',
       initialRoute: AppRouter.splash,
