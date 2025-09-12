5975cc694fb8b91c4bb9a53433e94db6a570de64
sandeeffendi <sande.effendi@gmail.com>
2025-09-13 02:19:14 +0700

    feat: implement app material theme

--- lib/core/providers/theme_provider.dart
+++ lib/core/providers/theme_provider.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
-class ThemeProvider extends ChangeNotifier {}
+class ThemeProvider extends ChangeNotifier {
+  bool _isDark = false;
+  bool get isDark => _isDark;
+
+  void themeOnChanged() {
+    _isDark = !_isDark;
+    notifyListeners();
+  }
+}
