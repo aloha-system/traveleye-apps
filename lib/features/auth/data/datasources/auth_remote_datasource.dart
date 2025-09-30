b1c5ba35b4b81e6f484c486fd9c87258e69f4bf7
sandeeffendi <sande.effendi@gmail.com>
2025-10-01 00:30:30 +0700

    feat(auth): create 'create account' use case

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -5,6 +5,27 @@ class AuthRemoteDatasource {
 
   const AuthRemoteDatasource(this.firebaseAuth);
 
+  // firebase authentication create accound with email and password
+  Future<User> createAccount({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      final result = await firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await result.user!.updateDisplayName(name);
+
+      return result.user!;
+    } on FirebaseAuthException catch (e) {
+      throw Exception('Failed to create an account. ${e.code}');
+    }
+  }
+
+  // firebase authentication sign in with email and password
   Future<User> signIn(String email, String password) async {
     try {
       final result = await firebaseAuth.signInWithEmailAndPassword(
