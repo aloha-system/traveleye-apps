8f40be70709a29fdb845b04130b136245673faa6
sandeeffendi <sande.effendi@gmail.com>
2025-09-30 23:26:20 +0700

    feat(auth): initilazing firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:boole_apps/app/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const AppWidget());
 }
