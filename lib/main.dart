20b2ee2e72f487583381150d4a7222d606963a22
sandeeffendi <sande.effendi@gmail.com>
2025-10-08 00:39:24 +0700

    feat(auth): updating login page with sign in use case

--- lib/main.dart
+++ lib/main.dart
@@ -7,5 +7,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const AppWidget());
+  // todo: create dependecies injection for project
+
+  runApp(AppWidget());
 }
