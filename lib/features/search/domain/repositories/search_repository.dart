57585e7d595b7bc6fc50e76b829942e5264c8784
sandeeffendi <sande.effendi@gmail.com>
2025-10-11 15:09:10 +0700

    feat(destination): refactor destination app router and injection. Deleting search feature.

--- lib/features/search/domain/repositories/search_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:boole_apps/features/search/domain/entities/destination.dart';
-import 'package:boole_apps/features/search/domain/usecases/search_destinations_usecase.dart';
-
-/// Kontrak repository untuk fitur Search
-/// yang menghubungkan antara Domain ↔ Data layer
-abstract class SearchRepository {
-  /// Cari destinasi wisata berdasarkan parameter
-  Future<List<Destination>> search(SearchParams params);
-}
