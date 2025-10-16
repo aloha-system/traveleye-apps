6e459fad5557847f3b7016904adfa945f8ebb9ef
sandeeffendi <sande.effendi@gmail.com>
2025-10-16 20:15:40 +0700

    fix(main): refactor: move api keys to envied

---
+++ lib/env/env.dart
@@ -0,0 +1,15 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+final class Env {
+  @EnviedField(varName: 'SUPABASE_API_KEY')
+  static const String supabaseApiKey = _Env.supabaseApiKey;
+  
+  @EnviedField(varName: 'FIREBASE_ANDROID_API_KEY')
+  static const String firebaseAndroidApiKey = _Env.firebaseAndroidApiKey;
+  
+  @EnviedField(varName: 'FIREBASE_IOS_API_KEY')
+  static const String firebaseIosApiKey = _Env.firebaseIosApiKey;
+}
