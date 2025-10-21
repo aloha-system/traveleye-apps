835f957cf5ac4011581393e103aa723c21596806
sandeeffendi <sande.effendi@gmail.com>
2025-10-21 12:36:07 +0700

    feat(emergency): initialize emergency feature

---
+++ lib/features/emergency/data/datasources/e_services_remote_datasource.dart
@@ -0,0 +1,9 @@
+class EServicesRemoteDatasource {
+  final String apiKey;
+  final String baseUrl;
+
+  const EServicesRemoteDatasource({
+    required this.baseUrl,
+    required this.apiKey,
+  });
+}
